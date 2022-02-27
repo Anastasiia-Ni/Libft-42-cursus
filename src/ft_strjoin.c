6d978b9dbcb280aa0c3af6e3d33469b00f91d8fc
Anastasiia-Ni <stasy247@mail.ru>
2022-02-27 11:09:16 -0800

    fixed strjoin and strncmp

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -26,9 +26,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!str)
 		return (NULL);
 	while (s1[++i])
-	{
 		str[i] = s1[i];
-	}
 	while (s2[++j])
 		str[i + j] = s2[j];
 	str[i + j] = '\0';
